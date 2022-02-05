f9b6c279deb1eb9de9aa3a7ec4272ca8c3f10b69
ltuczak <logan.tucz@gmail.com>
2022-02-05 18:05:53 -0500

    contact page new line fix v2

--- src/contactPage.js
+++ src/contactPage.js
@@ -4,13 +4,19 @@ const contactPage = function() {
     const container = document.createElement('div');
     container.id = 'container';
 
-    const p = document.createElement('p');
-    p.innerHTML = `Phone: +1 234 567 8910
-    Email: patspotatopub@gmail.com
-    Mailing Address: 123 Potato Place, Boise, Idaho`;
+    const p1 = document.createElement('p');
+    p1.innerHTML = `Phone: +1 234 567 8910`;
+
+    const p2 = document.createElement('p');
+    p2.innerHTML = `Email: patspotatopub@gmail.com`;
+
+    const p3 = document.createElement('p');
+    p3.innerHTML = `Mailing Address: 123 Potato Place, Boise, Idaho`;
 
     content.appendChild(container);
-    container.appendChild(p);
+    container.appendChild(p1);
+    container.appendChild(p2);
+    container.appendChild(p3);
 };
 
 
